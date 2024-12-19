886452ab94f964d0f4ff28396b1c8bb31c9ce657
Nick B <ngbaker99@gmail.com>
2024-12-19 17:28:58 +0000

    page header

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header>
+      <div className="logo">
+        <img src="images/smile_emoji_jester_small.png" alt="wink icon" />
+        Jokester
+      </div>
+    </header>
+  )
+}
+
+export default Header
