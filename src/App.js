b723b5a66ae2a095b95e31b36041291c3eadc945
Nick B <ngbaker99@gmail.com>
2024-12-23 12:09:21 +0000

    new joke button, started logic of displaying jokes

--- src/App.js
+++ src/App.js
@@ -6,26 +6,43 @@ import Header from './components/Header';
 import SpeechBubble from './components/SpeechBubble';
 
 function App() {
-  const [jokeVisible, setJokeVisible] = useState(false);
+  const [joke, setJoke] = useState(null);
   const [feedback, setFeedback] = useState(null);
+
+  const fetchJoke = () => {
+
+  }
 
   return (
     <div className="App">
       <Header />
       <div className="content">
-        <SpeechBubble 
-          title="Welcome to Jokester!"
-          subTitle="Your Ultimate Source for Laughter"
-          speechText="Looking for a good laugh? You've come to the right place! Jokester is a random joke generator that serves up over 200,000 hilarious jokes at the click of a button. Whether you need a quick chuckle, a witty one-liner, or a pun-packed punchline, Jokester has you covered.<br /><br />But that's not all! We believe that humor is best when shared. That's why we let you rate each joke, helping others discover the funniest ones. Get ready to laugh, rate, and repeat!"
-          buttonText="Get Started"
-        />
-        <div className="feedback">
-          <h3>So what did you think?</h3>
-          <div className="feedback-btns">
-            <FeedbackButton type="like" feedback={feedback} setFeedback={setFeedback} />
-            <FeedbackButton type="dislike" feedback={feedback} setFeedback={setFeedback} />
-          </div>
-        </div>
+        {joke ? (
+          <>
+            <SpeechBubble 
+              title="Here's your joke"
+              speechText={joke.text}
+            />
+            <button className="generate-btn">
+              <img src="images/sync.png" alt="re-generate icon" />
+              New Joke
+            </button>
+            <div className="feedback">
+              <h3>So what did you think?</h3>
+              <div className="feedback-btns">
+                <FeedbackButton type="like" feedback={feedback} setFeedback={setFeedback} />
+                <FeedbackButton type="dislike" feedback={feedback} setFeedback={setFeedback} />
+              </div>
+            </div>
+          </>
+        ) : (
+          <SpeechBubble 
+            title="Welcome to Jokester!"
+            subTitle="Your Ultimate Source for Laughter"
+            speechText="Looking for a good laugh? You've come to the right place! Jokester is a random joke generator that serves up over 200,000 hilarious jokes at the click of a button. Whether you need a quick chuckle, a witty one-liner, or a pun-packed punchline, Jokester has you covered.<br /><br />But that's not all! We believe that humor is best when shared. That's why we let you rate each joke, helping others discover the funniest ones. Get ready to laugh, rate, and repeat!"
+            buttonText="Get Started"
+          />
+        )}
       </div>
     </div>
   );
