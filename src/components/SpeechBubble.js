5d4dd896a3c8f69be5f1121a586fe5a85bd53cd2
Nick B <ngbaker99@gmail.com>
2025-01-06 15:53:34 +0000

    copy to clipboard button done

--- src/components/SpeechBubble.js
+++ src/components/SpeechBubble.js
@@ -1,16 +1,38 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const SpeechBubble = ({ speechText }) => {
+  // tracks if joke has been copied to clipboard
+  const [jokeCopied, setJokeCopied] = useState(false);
+
+  // when user clicks copy button add the joke text to users clipboard
+  const handleCopy = () => {
+    navigator.clipboard.writeText(speechText);
+    
+    setJokeCopied(true);
+  }
+
+  // reset copy status if new joke is loaded
+  useEffect(() => {
+    setJokeCopied(false);
+  }, [speechText]);
+
   return (
     <div className="speech-bubble">
       <div className="inner">
         <h1 className="title">Here's your joke</h1>
         <h2 className="sub-title">Prepare to be amused!</h2>
-        <p className="speech-text">{speechText}</p>
-        <button className="copy-btn">
-          <img src="images/copy.png" alt="copy icon" />
-          Copy joke
-        </button>
+        <p id="jokeText" className="speech-text">{speechText}</p>
+        {jokeCopied ? (
+          <button className="copy-btn" onClick={handleCopy}>
+            <img src="images/check.png" alt="tick icon" />
+            Copied
+          </button>
+        ) : (
+          <button className="copy-btn" onClick={handleCopy}>
+            <img src="images/copy.png" alt="copy icon" />
+            Copy joke
+          </button>
+        )}
       </div>
     </div>
   )
